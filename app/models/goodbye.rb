a1b7d96963717b4437ccbc51b8ed0d9458a9123f
sleeping-lion <toughjjh@gmail.com>
2013-12-28 22:09:32 +0900

    gg

--- app/models/goodbye.rb
+++ app/models/goodbye.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
 class Goodbye < ActiveRecord::Base
-  attr_accessible :title
   validates_presence_of :title    
 end
--- app/models/improve.rb
+++ app/models/improve.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
 class Improve < ActiveRecord::Base
-  attr_accessible :title  
   validates_presence_of :title,:content
 end
